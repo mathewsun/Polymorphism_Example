73cfa4b07518d246abbfeac541527ea8d011400e
Matthew <mathew.dubrovin@gmail.com>
2022-10-28 20:44:12 +0300

    create new project

---
+++ Polymorphism_example/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Polymorphism_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
